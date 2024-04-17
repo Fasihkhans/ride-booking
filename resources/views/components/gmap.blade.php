9cee1ecfb26c575e7d72d764f455450f425eed52
fasihkhan <fasihkhan068@gmail.com>
2024-04-18 01:17:52 +0500

    "Update gmap.blade.php with null check for window.google && window.google.maps; add socket.io script to booking.blade.php"
    This diff includes changes to the gmap.blade.

--- resources/views/components/gmap.blade.php
+++ resources/views/components/gmap.blade.php
@@ -1,18 +1,20 @@
 <div>
     <div id="map" {{ $attributes->merge(['class' => 'w-full py-4 h-80 rounded-xl']) }}></div>
       <script>
-        const apiKey = "{{ env('GOOGLE_MAPS_KEY') }}";
+        if (!window.google || !window.google.maps) {
+        var apiKey = "{{ env('GOOGLE_MAPS_KEY') }}";
         (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
             key: apiKey,
             v: "weekly",
         });
-            let map;
+        }
+            var map;
            async function initMap() {
-                    const { Map } = await google.maps.importLibrary("maps"); // Destructure the 'maps' object
-                    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+                    var { Map } = await google.maps.importLibrary("maps"); // Destructure the 'maps' object
+                    var { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-                    const directionsService = new google.maps.DirectionsService();
-                    const directionsRenderer = new google.maps.DirectionsRenderer({
+                    var directionsService = new google.maps.DirectionsService();
+                    var directionsRenderer = new google.maps.DirectionsRenderer({
                         suppressMarkers: true,
                         polylineOptions: {
                             strokeColor: '#FFB800',
@@ -27,20 +29,20 @@
                         mapId: "294912106c2419c5",
                     });
 
-                    const origin = @json([$origin->latitude,$origin->longitude]).toString();
-                    const destination = @json([$destination->latitude,$destination  ->longitude]).toString();
-                    const originFlagImg = document.createElement("img");
-                    const destinationFlagImg = document.createElement("img");
+                    var origin = @json([$origin->latitude,$origin->longitude]).toString();
+                    var destination = @json([$destination->latitude,$destination  ->longitude]).toString();
+                    var originFlagImg = document.createElement("img");
+                    var destinationFlagImg = document.createElement("img");
                     originFlagImg.src = @json(asset('assets/svg/origin-pointer.svg')).toString();
                     destinationFlagImg.src = @json(asset('assets/svg/destination-pointer.svg')).toString();
-                    const originMarker = new google.maps.marker.AdvancedMarkerElement({
+                    var originMarker = new google.maps.marker.AdvancedMarkerElement({
                         position: { lat: @json($origin->latitude), lng: @json($origin->longitude) },
                         map: map,
                         title: 'Origin',
                         content: originFlagImg
                     });
 
-                    const destinationMarker = new google.maps.marker.AdvancedMarkerElement({
+                    var destinationMarker = new google.maps.marker.AdvancedMarkerElement({
                         position: { lat: @json($destination->latitude), lng: @json($destination->longitude) },
                         map: map,
                         title: 'Destination',
--- resources/views/livewire/customers/booking.blade.php
+++ resources/views/livewire/customers/booking.blade.php
@@ -186,10 +186,10 @@ class extends Component
 
         </x-booking-card>
         {{-- @script --}}
+        <script src="https://cdn.socket.io/4.7.5/socket.io.min.js" integrity="sha384-2huaZvOR9iDzHqslqwpR87isEmrfxqyWOF7hr7BY6KG0+hVKLoEXMPUJw3ynWuhO" crossorigin="anonymous"></script>
         {{-- @endscript --}}
         @script
-        <script src="https://cdn.socket.io/4.7.5/socket.io.min.js" integrity="sha384-2huaZvOR9iDzHqslqwpR87isEmrfxqyWOF7hr7BY6KG0+hVKLoEXMPUJw3ynWuhO" crossorigin="anonymous"></script>
-    <script>
+        <script>
         $(document).ready(function() {
             $('#cancel').click(function() {
                 $wire.dispatch('cancelRide');
